8e0e2146422cf0adcebad22595aeb2e7f5e03586
Zykhor <zuxoran@gmail.com>
2025-05-24 16:28:51 +0300

    test

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import { createRoot } from 'react-dom/client';
+
+import './styles/index.css';
+import './styles/todo-list.css';
+import './styles/filters.css';
+
+import { App } from './App';
+
+const container = document.getElementById('root');
+
+if (!container) {
+  throw new Error('Root container not found');
+}
+
+const root = createRoot(container);
+
+root.render(<App />);
